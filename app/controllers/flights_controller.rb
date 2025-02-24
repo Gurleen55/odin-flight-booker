2b92a876febf187551d401a714ed1450d3d20cd4
Gurleen55 <gurleencan6@gmail.com>
2025-02-24 04:32:20 -0400

    implement search bar

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,15 @@
 class FlightsController < ApplicationController
   def index
     @available_airports = Airport.all.pluck(:name, :id)
-    @flights = Flight.search(params[:departure_airport_id]) || []
+    @available_times = Flight.all.map do |flight|
+      [ flight.formatted_time, flight.time  ]
+    end
+    @flights = Flight.search(flight_params) || []
   end
 
   private
 
   def flight_params
-    params.expect(:flight [:duration, :time, :departure_airport_id, :arrival_airport_id])
+    params.permit(:time, :departure_airport_id, :arrival_airport_id, :number_of_passengers)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,8 +2,22 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
-  def self.search(departure_airport_id)
-      flights = Flight.all
-      flights = flights.where(departure_airport_id: departure_airport_id) if departure_airport_id.present?
+  def self.search(params)
+      params = params.permit(:departure_airport_id, :arrival_airport_id, :time)
+      conditions = params.slice(:departure_airport_id, :arrival_airport_id).delete_if { |_, v| v.blank? }
+
+      if params[:time].present?
+        time = Time.zone.parse(params[:time]) rescue nil
+        if time
+          start_time = time.beginning_of_day
+          end_time = time.end_of_day
+          conditions[:time] = start_time..end_time
+        end
+      end
+      Flight.where(conditions)
+  end
+
+  def formatted_time
+    time.strftime("%d/%m/%Y") if time.present?
   end
 end
